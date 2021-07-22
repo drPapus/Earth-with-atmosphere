1766f24b402b5f2989f5488e5443d7f69b53309f
anton <antonklimovv@gmail.com>
2021-07-22 20:03:13 +0300

    Moon & Mars

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function main() {
   const near = 0.1;
   const far = 1000;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 10, 20);
+  camera.position.set(0, 0, 20);
 
   const controls = new THREE.OrbitControls(camera, canvas);
   controls.target.set(0, 0, 0);
@@ -36,7 +36,8 @@ function main() {
     }))
     scene.add( sphere );
 
-    //Atmosphere
+
+  //Atmosphere
 
     const atmosphere = new THREE.Mesh(new THREE.
       SphereGeometry(5.15, 50, 50), new THREE.
@@ -46,6 +47,31 @@ function main() {
         ),transparent: true, opacity: 0.5, color: 0xFFFFFF
       }))
       scene.add( atmosphere );
+
+
+
+  //Mars
+
+  const mars = new THREE.Mesh(new THREE.
+    SphereGeometry(5, 50, 50), new THREE.
+    MeshBasicMaterial({
+      map: new THREE.TextureLoader().load('./img/mars.jpg'),
+    }))
+
+    mars.position.set(40,-10,-55)
+    scene.add( mars );
+
+
+    //Moon
+
+    const moon = new THREE.Mesh(new THREE.
+      SphereGeometry(5, 50, 50), new THREE.
+      MeshBasicMaterial({
+        map: new THREE.TextureLoader().load('./img/moon.jpg'),
+      }))
+  
+      moon.position.set(-40, 10, -55)
+      scene.add( moon );
 
 
 
@@ -98,6 +124,10 @@ function main() {
       
     atmosphere.rotation.y += 0.001;
     sphere.rotation.y += 0.0005;
+    mars.rotation.y += 0.001;
+    moon.rotation.y += 0.002;
+    
+
     
 
 
